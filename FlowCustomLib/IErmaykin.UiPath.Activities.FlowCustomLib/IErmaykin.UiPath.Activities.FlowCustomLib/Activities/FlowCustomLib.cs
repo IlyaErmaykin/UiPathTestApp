be34d40769bb36097bffafb9c697a30ac426bfbd
IErmaykin <IErmaykin@ibs.ru>
2019-08-02 18:55:09 +0400

    #10 - FlowCustomLib - Промежуточный коммит;  - Создан проект FlowCustomLib;  - Реализован таймер;  - Начато подключение таймера в основной логике программы;

---
+++ FlowCustomLib/IErmaykin.UiPath.Activities.FlowCustomLib/IErmaykin.UiPath.Activities.FlowCustomLib/Activities/FlowCustomLib.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Activities;
+using System.Timers;
+using IErmaykin.UiPath.Activities.FlowCustomLib.Activities;
+
+namespace IErmaykin.UiPath.Activities.FlowCustomLib
+{
+    public class FlowCustomLib : CodeActivity
+    {
+        /// <summary>
+        /// Time property
+        /// </summary>
+        public InArgument<double> Time { get; set; }
+        
+        /// <summary>
+        /// Result flag property
+        /// </summary>
+        public InArgument<bool> ResultFlag { get; set; }
+
+        // Возможно не пригодится..
+        //public OutArgument<> { get; set; } 
+        
+        /// <summary>
+        /// Execute function
+        /// </summary>
+        /// <param name="context"></param>
+        protected override void Execute(CodeActivityContext context)
+        {
+            double inputTime = Time.Get(context);
+            bool resultFlag = ResultFlag.Get(context);
+
+            //Activities.Timer timer = new Activities.Timer();
+            
+
+            //timer.SetTimer(inputTime, resultFlag);
+            //timer.Stop();
+            //timer.Dispose();
+        }
+    }
+}
