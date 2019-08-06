40392a4fcb8638f93bf356409e513a5186d975de
IErmaykin <IErmaykin@ibs.ru>
2019-08-06 16:35:38 +0400

    #11 - FlowCustomLib - Промежуточный коммит;  - Таймер заменен на ожидание потока;  - Реализована библиотека, ожидающая в дочернем потоке, которая по истечении времен присылвает исключение;

--- FlowCustomLib/IErmaykin.UiPath.Activities.FlowCustomLib/IErmaykin.UiPath.Activities.FlowCustomLib/Activities/FlowCustomLib.cs
+++ FlowCustomLib/IErmaykin.UiPath.Activities.FlowCustomLib/IErmaykin.UiPath.Activities.FlowCustomLib/Activities/FlowCustomLib.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Activities;
-using System.Timers;
+using System.Threading;
 using IErmaykin.UiPath.Activities.FlowCustomLib.Activities;
-
+    
 namespace IErmaykin.UiPath.Activities.FlowCustomLib
 {
     public class FlowCustomLib : CodeActivity
@@ -11,15 +11,17 @@ namespace IErmaykin.UiPath.Activities.FlowCustomLib
         /// Time property
         /// </summary>
         public InArgument<double> Time { get; set; }
-        
+
         /// <summary>
         /// Result flag property
         /// </summary>
         public InArgument<bool> ResultFlag { get; set; }
 
-        // Возможно не пригодится..
-        //public OutArgument<> { get; set; } 
-        
+        /// <summary>
+        /// Second thread field
+        /// </summary>
+        public OutArgument<Thread> SecondThread { get; set; }
+
         /// <summary>
         /// Execute function
         /// </summary>
@@ -29,12 +31,29 @@ namespace IErmaykin.UiPath.Activities.FlowCustomLib
             double inputTime = Time.Get(context);
             bool resultFlag = ResultFlag.Get(context);
 
-            //Activities.Timer timer = new Activities.Timer();
+            try
+            {
+                var secodTread = Flows.createFlows(new ParameterizedThreadStart(this.runFunction), inputTime, resultFlag);
+                SecondThread.Set(context, secodTread);
+            }
+            catch (Exception e) { }
             
+        }
 
-            //timer.SetTimer(inputTime, resultFlag);
-            //timer.Stop();
-            //timer.Dispose();
+        /// <summary>
+        /// Run function
+        /// </summary>
+        /// <param name="obj"></param>
+        public void runFunction(object obj)
+        {
+            var objArray = (object[])obj;
+            var thread = (Thread)objArray[0];
+            double inputTime = (double)objArray[1];
+            bool resultFlag = (bool)objArray[2];
+
+            Thread.Sleep((int)inputTime);
+            thread.Abort();
+
         }
     }
 }
---
+++ FlowCustomLib/IErmaykin.UiPath.Activities.FlowCustomLib/IErmaykin.UiPath.Activities.FlowCustomLib/Activities/Flows.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading;
+
+namespace IErmaykin.UiPath.Activities.FlowCustomLib.Activities
+{
+    /// <summary>
+    /// Flow class
+    /// </summary>
+    public class Flows
+    {
+        /// <summary>
+        /// Create flows function
+        /// </summary>
+        /// <param name="flowCustomLib"></param>
+        /// <param name="inputTime"></param>
+        /// <param name="resultFlag"></param>
+        public static Thread createFlows(ParameterizedThreadStart parameterizedThreadStart, double inputTime, bool resultFlag)
+        {
+            var options = new object[3];
+            options[0] = Thread.CurrentThread;
+            options[1] = inputTime;
+            options[2] = resultFlag;
+
+            Thread parralelThread = new Thread(parameterizedThreadStart);
+            parralelThread.IsBackground = true;
+            parralelThread.Start(options);
+
+            return parralelThread;
+        }
+    }
+}
